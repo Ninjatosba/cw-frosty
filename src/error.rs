08abff0c93a40732c0a6e7ea27f74ca846bc77e7
Ninjatosba <deniz@deuslabs.fi>
2023-02-15 12:44:51 +0300

    Tarpaulin doc is added and unused errors removed

--- src/error.rs
+++ src/error.rs
@@ -15,9 +15,6 @@ pub enum ContractError {
     #[error("{0}")]
     Payment(#[from] PaymentError),
 
-    #[error("No rewards accrued")]
-    NoRewards {},
-
     #[error("Reward end time cannot be in the past")]
     InvalidRewardEndTime {},
 
@@ -30,17 +27,8 @@ pub enum ContractError {
     #[error("Unauthorized")]
     Unauthorized {},
 
-    #[error("Do not send native funds")]
-    DoNotSendFunds {},
-
-    #[error("Amount required")]
-    AmountRequired {},
-
     #[error("Decrease amount exceeds user balance: {0}")]
     DecreaseAmountExceeds(Uint128),
-
-    #[error("Wait for the unbonding")]
-    WaitUnbonding {},
 
     #[error("No claim for user")]
     NoClaim {},
@@ -60,23 +48,8 @@ pub enum ContractError {
     #[error("Please send right denom and funds")]
     NoFund {},
 
-    #[error("Address validation failed")]
-    InvalidAddress {},
-
-    #[error("Invalid token type")]
-    InvalidTokenType {},
-
-    #[error("Invalid config")]
-    InvalidConfig {},
-
     #[error("Invalid cw20 token address")]
     InvalidCw20TokenAddress {},
-
-    #[error("Denom not supperted")]
-    DenomNotSupported {},
-
-    #[error("Multiple tokens sent")]
-    MultipleTokensSent {},
 
     #[error("No Bond for duration sent")]
     NoBondForThisDuration {},
